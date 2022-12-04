ea09c007e3d52cb4067f9d25c20dbdd83dade350
Marylucy <marylucyatienoomenda.gmail.com>
2022-12-04 22:57:49 +0300

    commit assignment

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
-getCats(cats)
-})
+    getCats(cats)
+    })
 
-fetch("http://localhost:3000/cat")
-.then(res => res.json())
-.then(data => console.log(d))
+    const url = "http://localhost:3000/characters"
 
-function getCats(){
-
-fetch(url)
-.then(res => res.json())
-.then(data =>{
-    console.log(data)
-    data.forEach(cat => {
-        displayCats(cat)
+    function getCats(){
         
-    });
-})
-
-}
-
-{
-    function displayCats(cats) {
-        console.log(cats);
-        const name= document.createElement('p');
-        name.innerHTML= cat.name
-        const main= document.getElementById("main")
-        main.append(name)
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+         console.log(data)
+        data.forEach(element => {
+            displayCatsName(cat)
+            
+        });
+        })
     }
-}
+
+    
+    
+    {
+        function displayCatsName(cat) {
+            console.log(cat);
+            const name= document.createElement('p');
+            name.innerHTML= cat.name
+            const main= document.getElementById("main")
+            main.append(name)
+    
+            name.addEventListener("click", () => {
+                getSingleCat(cat)
+            })
+        }
+    }
+    
+    function getSingleCat(cat){
+        document.getElementById("catname").innerHTML = cat.name
+        document.getElementById("catimage").src = cat.image
+        const btn =document.getElementById("catlikes")
+        btn.addEventListener("click", () => {
+            cat.votes++
+            btn .textContent =`likes ${cat.votes}`
+        })
+
+        
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+
+
